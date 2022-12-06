183f7c9c3678a4f8bc983e6035effbc9e72bda8e
Livio Alvarenga <livioalvarenga@gmail.com>
2022-12-06 11:34:01 -0300

    add programação defensiva em _data

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -6,7 +6,8 @@ export class Negociacao {
     }
     // getters
     get data() {
-        return this._data;
+        const data = new Date(this._data.getTime()); // programação defensiva, se modificar a data modifica a copia da data, getTime() cria uma copia de this._data
+        return data;
     }
     get quantidade() {
         return this._quantidade;
@@ -21,13 +22,18 @@ export class Negociacao {
 /* Uma outra forma de fazer o construtor somente com public readonly
 export class Negociacao {
   constructor(
-    public readonly data: Date,
+    private _data: Date,
     public readonly quantidade: number,
     public readonly valor: number
   ) {}
 
   get volume(): number {
     return this.valor * this.quantidade;
+  }
+
+  get data(): Date {
+    const data = new Date(this._data.getTime()); // programação defensiva, se modificar a data modifica a copia da data, getTime() cria uma copia de this._data
+    return data;
   }
 }
 */
