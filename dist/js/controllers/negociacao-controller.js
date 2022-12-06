c745e05b5e77aa5f37815eeeba8fba4f446505b4
Livio Alvarenga <livioalvarenga@gmail.com>
2022-12-06 14:11:38 -0300

    add mensagem view

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,10 +1,12 @@
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
 export class NegociacaoController {
     constructor() {
         this.negociacoes = new Negociacoes(); // Como ja estou atribuindo Negociacoes(), não preciso tipar
         this.negociacoesView = new NegociacoesView("#negociacoesView");
+        this.mensagemView = new MensagemView("#mensagemView");
         this.inputData = document.querySelector("#data");
         this.inputQuantidade = document.querySelector("#quantidade");
         console.log(this.negociacoes.lista());
@@ -15,6 +17,7 @@ export class NegociacaoController {
         const negociacao = this.criaNegociação();
         this.negociacoes.adiciona(negociacao);
         this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update("Negociação adicionada com sucesso");
         this.limparForm();
     }
     criaNegociação() {
