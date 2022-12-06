a11921ebbe29d4f2fd2d55ac7ce824b9fe388936
Livio Alvarenga <livioalvarenga@gmail.com>
2022-12-06 15:12:25 -0300

    transformando view como abstract class e template como abstract metodo

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,5 @@
-export class View<T> {
+// abstract class não pode criar uma instancia dela, somente os filhos podem usar
+export abstract class View<T> {
   protected elemento: HTMLElement; // protected so eu View tenho acesso a este elemento, mas minhas filhas podem tocar
 
   constructor(seletor: string) {
@@ -9,7 +10,6 @@ export class View<T> {
     this.elemento.innerHTML = this.template(model);
   }
 
-  template(model: T): string {
-    throw Error("Classe filha precisa implementar o método template.");
-  }
+  // tornando o método template para abstract, se a classe filha não implementar o template nem realiza a compilação, dando erro em ambiente DEV.
+  abstract template(model: T): string;
 }
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,11 +1,9 @@
+// abstract class não pode criar uma instancia dela, somente os filhos podem usar
 export class View {
     constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
     update(model) {
         this.elemento.innerHTML = this.template(model);
-    }
-    template(model) {
-        throw Error("Classe filha precisa implementar o método template.");
     }
 }
