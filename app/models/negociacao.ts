bfc19843b7c543fba2c60053702b57ff0a978efa
Livio Alvarenga <livioalvarenga@gmail.com>
2022-12-06 07:02:08 -0300

    add atributo pivate em Ts

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,25 +1,25 @@
 export class Negociacao {
-  #data;
-  #quantidade;
-  #valor;
+  private _data; // private para informar atributo privado no TS, posso também usar o public para atributos públicos
+  private _quantidade;
+  private _valor;
 
   constructor(data, quantidade, valor) {
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
   }
 
   // getters
   get data() {
-    return this.#data;
+    return this._data;
   }
   get quantidade() {
-    return this.#quantidade;
+    return this._quantidade;
   }
   get valor() {
-    return this.#valor;
+    return this._valor;
   }
   get volume() {
-    return this.#valor * this.#quantidade;
+    return this._valor * this._quantidade;
   }
 }
